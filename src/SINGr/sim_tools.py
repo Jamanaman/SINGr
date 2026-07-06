7c13cba939445c07cd16e7bc4d7811044cc4b715
Jamanaman <tmdamjanovic@gmail.com>
2026-07-06 23:07:43 +0200

    implementation of subckt line search util for code clarity

--- src/SINGr/sim_tools.py
+++ src/SINGr/sim_tools.py
@@ -13,3 +13,10 @@ def make_pwl_from_bitstream(bitstream:List[int], period:float) -> List[Tuple[flo
         pwl_vals.append((i*period, v_bit))
         pwl_vals.append((i*period+period*0.99, v_bit))
     return pwl_vals
+
+def find_subckt_line(subcircuit_card: str, return_string: bool = False) -> str|Tuple[int,int]:
+    sckt_line_start = subcircuit_card.find('.SUBCKT')
+    sckt_line_end = subcircuit_card.find('\n', sckt_line_start)
+    if return_string:
+        return subcircuit_card[sckt_line_start:sckt_line_end]
+    return (sckt_line_start, sckt_line_end)
