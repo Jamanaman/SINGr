8e4d93a7db53f70d3d4bb0152ff929657e0106ee
Jamanaman <tmdamjanovic@gmail.com>
2026-08-27 12:42:38 +0200

    Add daisy chain example

--- examples/daisy_with_ibis_models.py
+++ examples/daisy_with_ibis_models.py
@@ -31,22 +31,45 @@ if __name__ == '__main__':
         lib_path=Path(__file__).parent
         )
     
-    comp = Composition(
-        name='DDR3_Test_point_to_point',
-        net_tree=CommunicationNet(
-            t_line_nodes_in=[sender_1, sender_2, sender_2],
-            t_line_nodes_out=[receiver_1, receiver_1, receiver_1],
+    net2 = CommunicationNet(
+            t_line_nodes_in=['net1_out1', 'net1_out2', 'gnd'],
+            t_line_nodes_out=[receiver_1, receiver_1, 'gnd'],
             transmission_line=tline,
-            l_tline_m=50e-3,
+            l_tline_m=10e-3,
+            clock_frequencies_Hz=[800e6, 800e6, None],
+            delays_s=[None, None, None],
+            v_logic=[(0, 1.2), (0, 1.2), None],
+            stimuli=[None, None, None],
+            terminations_start=[None, None, None],
+            terminations_end=[None, None, None]
+    )
+
+    net1 = CommunicationNet(
+            t_line_nodes_in=[sender_1, sender_2, sender_2],
+            t_line_nodes_out=[net2, 'net1_out2', receiver_1],
+            transmission_line=tline,
+            l_tline_m=10e-3,
             clock_frequencies_Hz=[800e6, 800e6, 800e6],
             delays_s=[None, None, None],
             v_logic=[(0, 1.2), (0, 1.2), (0, 1.2)],
             stimuli=["TRIG", "LOW", "LOW"],
             terminations_start=[None, None, None],
             terminations_end=[None, None, None]
-        ),
+    )
+
+    comp = Composition(
+        name='DDR3_Test_daisychain',
+        net_tree=net1,
         analyses=['EYE', 'WAVEFORM']
     )
 
+    analysis_config = {'key': 'pin_out_1_0',     
+                'clock_frequency': comp.net_tree.clock_frequencies_Hz[0],
+                'v_high': comp.net_tree.v_logic[0][1],
+                'v_low':  comp.net_tree.v_logic[0][0],
+                'threshold_pct': 0.9, 
+                'statistical_jitter_s': 0,
+                'visualise': True}
+
     results = compose_and_run_simulation(comp)
-    analyse_simulation_results(comp, results, visualise=True)
+    analyse_simulation_results(comp, results, visualise=True, eye_config=analysis_config, waveform_config=analysis_config)
